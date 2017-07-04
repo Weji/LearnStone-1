cb45602758eca3372bc80882d3780d2476def08a
Louis Mantopoulos <Louis@MacBook-Pro-de-Louis.local>
2017-07-04 15:31:49 +0200

    MarcheSTP

--- Learnstone_BDD/bdd.sql
+++ Learnstone_BDD/bdd.sql
@@ -3,12 +3,18 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:8889
--- Généré le :  Mar 04 Juillet 2017 à 10:10
+-- Généré le :  Mar 04 Juillet 2017 à 15:19
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de données :  `LearnStone`
@@ -594,3 +600,7 @@ ALTER TABLE `PersonLesson`
 ALTER TABLE `QuestionAnswered`
   ADD CONSTRAINT `FK_QuestionAnswered_Person` FOREIGN KEY (`IdPerson`) REFERENCES `Person` (`IdPerson`),
   ADD CONSTRAINT `FK_QuestionAnswered_Question` FOREIGN KEY (`IdQuestion`) REFERENCES `Question` (`IdQuestion`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
