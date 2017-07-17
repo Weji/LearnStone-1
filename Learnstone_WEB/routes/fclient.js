32db30a56e4e21089d6fac8d91be697ee00b1624
Steven-K-JOHNSON <paradoxath@gmail.com>
2017-07-17 18:35:52 +0200

    Creation WS

--- Learnstone_WEB/routes/fclient.js
+++ Learnstone_WEB/routes/fclient.js
@@ -1,20 +1,473 @@
+
 var express = require('express');
 var session = require('express-session');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
-var sess;
 
+var connection = require("../connection");
 var router = express.Router();
 
-router.get('/', function(req, res) {
-   //res.sendFile(path.join(__dirname + '/views/generic.html'));
-sess = req.session;
-   if(sess.email) {
-        res.render('generic');
-   }
-   else {
-       res.redirect('/');
-   }
+
+router.post('/profilUpdater', function(req, res) {
+
+  if (req.body.isAnswerWell) {
+
+    connection.query(
+      'UPDATE Person ' +
+      'SET nbGoodAnswer = nbGoodAnswer + 1, nbQuestionAnswered = nbQuestionAnswered + 1 ' +
+      'WHERE idPerson = ?'
+
+        , req.body.idPerson, function(err, result) {
+
+          if (err) { res.json({result: false}); }
+          else { res.json({result: true}); }
+
+    });
+
+  }
+  else {
+
+    connection.query(
+      'UPDATE Person ' +
+      'SET nbQuestionAnswered = nbQuestionAnswered + 1 ' +
+      'WHERE idPerson = ?'
+
+        , req.body.idPerson, function(err, result) {
+
+          if (err) { res.json({result: false}); }
+          else { res.json({result: true}); }
+
+    });
+
+  }
+
+});
+
+router.post('/questionLog', function(req, res) {
+
+  var now = new Date();
+
+  var iQuestionAnswered = {
+          idPerson: req.body.idPerson,
+          idQuestion: req.body.idQuestion,
+          isAnswerWell: req.body.isAnswerWell,
+          dateAnswered: now
+      };
+
+  connection.query('INSERT INTO QuestionAnswered SET ?', iQuestionAnswered, function(err, result) {
+
+    if (err) { res.json({result: false}); }
+    else { res.json({result: true}); }
+
+  });
+
+});
+
+router.post('/getPerson', function(req, res) {
+
+
+  connection.query('SELECT * FROM Person WHERE idPerson = ?', req.body.idPerson, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getIdPerson', function(req, res) {
+
+
+  connection.query('SELECT idPerson FROM Person WHERE username = ? and password = ?', [req.body.username, req.body.password], function(err, result) {
+
+    if (err) { res.json({result: false}); }
+    else {
+
+        if (result.length != 0)
+            res.json(result);
+        else
+          res.json({result: false});
+
+      }
+
+  });
+
+});
+
+router.post('/createPerson', function(req, res) {
+
+  var iPerson = {
+          firstname: req.body.firstname ,
+          lastname: req.body.lastname,
+          nbGoodAnswer: 0,
+          nbQuestionAnswered: 0,
+          idRefRole: req.body.idRefRole ,
+          username: req.body.username,
+          password: req.body.password,
+          mail: req.body.mail
+      };
+
+  connection.query('INSERT INTO Person SET ?', iPerson, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else { res.json({result: true}); }
+
+  });
+
+});
+
+router.post('/isRealUsername', function(req, res) {
+
+
+  connection.query('SELECT username FROM Person WHERE username = ?', req.body.username, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0 && result[0].username == req.body.username)
+                res.json({result: true});
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getMinIdQuestion', function(req, res) {
+
+
+  connection.query('SELECT min(idQuestion) FROM Question', function(err, result) {
+
+    if (err) { res.json({result: false}); }
+    else {
+
+        if (result.length != 0)
+            res.json(result);
+        else
+          res.json({result: false});
+
+      }
+
+  });
+
+});
+
+router.post('/getMaxIdQuestion', function(req, res) {
+
+
+  connection.query('SELECT max(idQuestion) FROM Question', function(err, result) {
+
+    if (err) { res.json({result: false}); }
+    else {
+
+        if (result.length != 0)
+            res.json(result);
+        else
+          res.json({result: false});
+
+      }
+
+  });
+
+});
+
+router.post('/getQuestion', function(req, res) {
+
+
+  connection.query('SELECT * FROM Question WHERE idQuestion = ?', req.body.idQuestion, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getAnswerFromQuestion', function(req, res) {
+
+
+  connection.query('SELECT * FROM Answer WHERE idQuestion = ?', req.body.idQuestion, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getListNumberLesson', function(req, res) {
+
+
+  connection.query('SELECT numberLesson FROM Lesson ORDER BY numberLesson', function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLesson', function(req, res) {
+
+
+  connection.query('SELECT * FROM Lesson WHERE numberLesson = ?', req.body.numberLesson, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/lessonLog', function(req, res) {
+
+  var now = new Date();
+
+  var iPersonLesson = {
+          idPerson: req.body.idPerson,
+          idLesson: req.body.idLesson,
+          dateRead: now
+      };
+
+  connection.query('INSERT INTO PersonLesson SET ?', iPersonLesson, function(err, result) {
+
+    if (err) { res.json({result: false}); }
+    else { res.json({result: true}); }
+
+  });
+
+});
+
+router.post('/getLastReadLesson', function(req, res) {
+
+
+  connection.query(
+    'SELECT l.numberLesson  FROM PersonLesson i ' +
+    'INNER JOIN Lesson l ON i.idLesson = l.idLesson ' +
+    'WHERE idPerson = ? AND dateRead IN (SELECT max(dateRead) FROM PersonLesson WHERE idPerson = ?)',
+
+  [req.body.idPerson, req.body.idPerson], function(err, result) {
+
+        if (err) { console.log(err); res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblCardsSet', function(req, res) {
+
+
+  connection.query('SELECT lblCardsSet FROM Refcardsset WHERE idRefCardsSet = ?', req.body.idRefCardsSet, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblClass', function(req, res) {
+
+
+  connection.query('SELECT lblClass FROM Refclass WHERE idRefClass = ?', req.body.idRefClass, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblRace', function(req, res) {
+
+
+  connection.query('SELECT lblRace FROM Refrace WHERE idRefRace = ?', req.body.idRefRace, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblRarity', function(req, res) {
+
+
+  connection.query('SELECT lblRarity FROM Refrarity WHERE idRefRarity = ?', req.body.idRefRarity, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblRole', function(req, res) {
+
+
+  connection.query('SELECT lblRole FROM Refrole WHERE idRefRole = ?', req.body.idRefRole, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getLblType', function(req, res) {
+
+
+  connection.query('SELECT lblType FROM Reftype WHERE idRefType = ?', req.body.idRefType, function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getIdAndURLCard', function(req, res) {
+
+
+  connection.query('SELECT idCard, img FROM Card ORDER BY idCard', function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getFilteredCard', function(req, res) {
+
+
+  connection.query('SELECT * FROM Card ' +
+                   'WHERE name = ? AND manaCost = ? AND idRefRarity = ? AND idRefClass = ?',
+                   [req.body.name, req.body.manaCost, req.body.idRefRarity, req.body.idRefClass], function(err, result) {
+
+        if (err) { res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
+
+});
+
+router.post('/getFilteredWord', function(req, res) {
+
+  connection.query('SELECT * from Definition where word like ?', req.body.letter + '%', function(err, result) {
+
+        if (err) { console.log(err); res.json({result: false}); }
+        else {
+
+            if (result.length != 0)
+                res.json(result);
+            else
+              res.json({result: false});
+
+          }
+
+  });
 
 });
 
--- Learnstone_WEB/routes/questions.js
+++ Learnstone_WEB/routes/questions.js
@@ -1,20 +1,117 @@
+
 var express = require('express');
 var session = require('express-session');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var bodyParser = require('body-parser');
-var sess;
 
+var connection = require("../connection");
 var router = express.Router();
 
+
 router.get('/', function(req, res) {
-   //res.sendFile(path.join(__dirname + '/views/generic.html'));
-   sess = req.session;
-   if(sess.email) {
-        res.render('questions');
+
+    if (!req.session.username) { res.redirect('/'); }
+    else {
+
+          connection.query('SELECT * FROM Question ORDER BY questionText', function(err, rows, fields) {
+
+                    if (!err){
+
+                       let result;
+                       for (var i = 0; i < rows.length; i++) {result = rows;};
+                       res.render('questions', {result: result});
+
+                     }
+                     else{
+
+                       res.render('error', {err: err});
+
+                     }
+          });
+
+        }
+
+});
+
+router.post('/', function(req, res) {
+
+  if (!req.session.username) { res.redirect('/'); }
+  else {
+
+  //    if (req.body.attack == "") {req.body.attack = null ;}
+    //  if (req.body.class == "") {req.body.class = null ;}
+      //if (req.body.race == "") {req.body.race = null ;}
+      //if (req.body.health == "") {req.body.health = null ;}
+      //if (req.body.durability == "") {req.body.durability = null ;}
+
+      var iquestion = {
+              questionText: req.body.questiontext,
+              nbUse: req.body.nbuse,
+              nbResult: req.body.nbresult,
+              img: req.body.img
+          };
+
+      var ianswer = {
+              answerText: req.body.answertext,
+              isCorrectAnswer: req.body.iscorrectanswer,
+          };
+
+      var values; var val;
+
+      connection.query('INSERT INTO Question SET ?', iquestion, function(err, result) {
+
+
+            if (!err) {
+
+
+               values = [
+               [req.body.answerTextOne, 0, result.insertId],
+               [req.body.answerTextTwo, 0, result.insertId],
+               [req.body.answerTextThree, 0, result.insertId],
+               [req.body.answerTextFour, 0, result.insertId]
+             ];
+
+             val = [
+             ['Test', 0, 0],
+             ['Test', 0, 0]
+           ];
+
+
+            }
+            //else { res.render('error', {err: err}); }
+
+//            next();
+
+      });
+
+var sql = "INSERT INTO Answer (answerText, isCorrectAnswer, idQuestion) VALUES ?";
+var sqlx = "INSERT INTO answer VALUES ?";
+      connection.query(sqlx, [val], function (err, result) {
+
+            console.log(err);
+            console.log(values);
+            if (!err) { res.redirect('/'); }
+            else { res.render('error', {err: err}); }
+
+      });
+
    }
-   else {
-       res.redirect('/');
-   }
+
+});
+
+router.post('/delete', function(req, res) {
+
+  if(!req.session.username) { res.redirect('/'); }
+  else {
+// Voir QUESTIONSANSWERED
+    connection.query('DELETE FROM Question WHERE idQuestion = ?', req.body.id, function(err, result) {
+
+      if (!err) { res.redirect('/questions'); }
+      else { res.render('error', {err: err}); }
+
+    });
+
+  }
 
 });
 
